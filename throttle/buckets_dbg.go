a6c81c071e68c3c96836d42af9d349241539a013
zeptools <zeptools.io@gmail.com>
2025-10-24 12:25:28 -0400

    .

--- throttle/buckets_dbg.go
+++ throttle/buckets_dbg.go
@@ -11,7 +11,7 @@ func (s *BucketStore[K]) Cleanup(olderThan time.Duration, now time.Time) {
 	log.Printf("[DEBUG] cleaning expired buckets older than %v at %v", olderThan, now)
 	cleanCnt := 0
 	for gid, g := range s.groups {
-		log.Printf("[DEBUG] cleaning expired buckets in bucketgroup %q", gid)
+		log.Printf("[DEBUG] cleaning up bucketgroup %q", gid)
 		g.buckets.Range(func(id, value any) bool {
 			b := value.(*Bucket[K])
 
