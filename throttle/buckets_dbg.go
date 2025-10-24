242db99ae7d0aea8c2f7fe4dbd15a824ced57dd2
zeptools <zeptools.io@gmail.com>
2025-10-24 13:26:46 -0400

    .

--- throttle/buckets_dbg.go
+++ throttle/buckets_dbg.go
@@ -8,10 +8,10 @@ import (
 )
 
 func (s *BucketStore[K]) Cleanup(olderThan time.Duration, now time.Time) {
-	log.Printf("[DEBUG] cleaning expired buckets older than %v at %v", olderThan, now)
+	log.Printf("[DEBUG][Throttle] cleaning expired Buckets older than %v at %v", olderThan, now)
 	cleanCnt := 0
 	for gid, g := range s.groups {
-		log.Printf("[DEBUG] cleaning up bucketgroup %q", gid)
+		log.Printf("[DEBUG][Throttle] cleaning BucketGroup %q", gid)
 		g.buckets.Range(func(id, value any) bool {
 			b := value.(*Bucket[K])
 
@@ -20,23 +20,23 @@ func (s *BucketStore[K]) Cleanup(olderThan time.Duration, now time.Time) {
 			last := b.lastCheck
 			switch typedID := id.(type) {
 			case int64, int:
-				log.Printf("[DEBUG] inspecting bucket id=%d lastCheck=%v", typedID, last)
+				log.Printf("[DEBUG][Throttle] inspecting Bucket id=%d lastCheck=%v", typedID, last)
 			case string:
-				log.Printf("[DEBUG] inspecting bucket id=%q lastCheck=%v", typedID, last)
+				log.Printf("[DEBUG][Throttle] inspecting Bucket id=%q lastCheck=%v", typedID, last)
 			default:
-				log.Printf("[DEBUG] inspecting bucket id=%v lastCheck=%v", typedID, last)
+				log.Printf("[DEBUG][Throttle] inspecting Bucket id=%v lastCheck=%v", typedID, last)
 			}
 			b.mu.Unlock()
 
 			if now.Sub(last) > olderThan {
 				g.buckets.Delete(id)
 				cleanCnt++
-				log.Println("[DEBUG] bucket REMOVED")
+				log.Println("[DEBUG][Throttle] Bucket REMOVED")
 			} else {
-				log.Println("[DEBUG] keeping bucket")
+				log.Println("[DEBUG][Throttle] keeping Bucket")
 			}
 			return true // continue iteration
 		})
 	}
-	log.Printf("[DEBUG] %d buckets cleaned up", cleanCnt)
+	log.Printf("[DEBUG][Throttle] %d Buckets cleaned up", cleanCnt)
 }
