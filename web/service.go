e8bf5c4e20159d412c345afb02926d42c518fe0e
zeptools <zeptools.io@gmail.com>
2025-10-27 13:51:00 -0400

    .

--- web/service.go
+++ web/service.go
@@ -1,16 +1,32 @@
 package web
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+
+	"github.com/zeptools/gw-core/service"
+)
 
 type Service struct {
+	Ctx    context.Context    // Service Context
+	Cancel context.CancelFunc // Service Context CancelFunc
+	state  int                // internal service state
 	Server *http.Server
 }
 
-func NewService(addr string, router http.Handler) *Service {
+func NewService(parentCtx context.Context, addr string, router http.Handler) *Service {
+	svcCtx, svcCancel := context.WithCancel(parentCtx)
 	return &Service{
+		Ctx:    svcCtx,
+		Cancel: svcCancel,
+		state:  service.StateREADY,
 		Server: &http.Server{
 			Addr:    addr,
 			Handler: router,
 		},
 	}
+}
+
+func (s *Service) Start() {
+
 }
