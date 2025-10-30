c07fda9ef780d78a88d91c3e01f685798749a3ff
zeptools <zeptools.io@gmail.com>
2025-10-30 17:36:35 -0400

    .

--- db/sqldb/helpers.go
+++ db/sqldb/helpers.go
@@ -6,14 +6,14 @@ import (
 )
 
 func QueryItems[
-	M any, // Model Base Type
-	MP Scannable[M], // Model Pointer Type Implementing Scannable[M]
+M any,           // Model struct
+MP Scannable[M], // *Model Implementing Scannable[M]
 ](
 	ctx context.Context,
 	DBHandle DBHandle,
 	rawStmt string,
 	args ...any, // variadic
-) ([]M, error) {
+) ([]*M, error) { // Returns a Slice of Model-Pointers
 	rows, err := DBHandle.QueryRows(ctx, rawStmt, args...)
 	if err != nil {
 		return nil, err
@@ -22,41 +22,42 @@ func QueryItems[
 }
 
 func RowsToNewItems[
-	M any, // Model Base Type
-	MP Scannable[M], // Model Pointer Type Implementing Scannable[M]
-](rows Rows) ([]M, error) {
-	var items []M
+M any,           // Model struct
+MP Scannable[M], // *Model Implementing Scannable[M]
+](rows Rows) ([]*M, error) { // Returns a Slice of Model-Pointers
+	var itemPtrs []*M
 	for rows.Next() {
 		var item M     // struct with zero values for the fields
 		p := MP(&item) // p is *M, which satisfies targetFieldsProvider interface
+		// Scan the Fields of Each Row to the Fields of the new struct of the Model
 		if err := rows.Scan(p.TargetFields()...); err != nil {
 			return nil, fmt.Errorf("scan failed: %v", err)
 		}
-		items = append(items, item)
+		itemPtrs = append(itemPtrs, &item) // Collect the pointers
 	}
 	if err := rows.Err(); err != nil {
 		return nil, fmt.Errorf("error during iterating rows: %v", err)
 	}
-	return items, nil
+	return itemPtrs, nil
 }
 
 func QueryItem[
-	M any, // Model Base Type
-	MP Scannable[M], // Model Pointer Type Implementing Scannable[M]
+M any,           // Model struct
+MP Scannable[M], // *Model Implementing Scannable[M]
 ](
 	ctx context.Context,
 	DBHandle DBHandle,
 	rawStmt string,
 	args ...any, // variadic
-) (*M, error) {
+) (*M, error) { // Returns the Pointer to the Newly Created Item
 	row := DBHandle.QueryRow(ctx, rawStmt, args...)
 	return RowToNewItem[M, MP](row)
 }
 
 func RowToNewItem[
-	M any, // Model Base Type
-	MP Scannable[M], // Model Pointer Type Implementing Scannable[M]
-](row Row) (*M, error) {
+M any,           // Model struct
+MP Scannable[M], // *Model Implementing Scannable[M]
+](row Row) (*M, error) { // Returns the Pointer to the Newly Created Item
 	var item M     // struct with zero values for the fields
 	p := MP(&item) // p is *M, which satisfies targetFieldsProvider interface
 	err := row.Scan(p.TargetFields()...)
