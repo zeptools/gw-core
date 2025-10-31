fc83f55385637425ef356c1b5a50a69b5cff92ce
zeptools <zeptools.io@gmail.com>
2025-10-31 14:21:42 -0400

    .

--- orm/collection.go
+++ orm/collection.go
@@ -1,5 +1,7 @@
 package orm
 
+import "fmt"
+
 type ModelCollection[MP Identifiable[ID], ID comparable] struct {
 	Map   map[ID]MP
 	Order []ID // optional: only populated if you care about iteration order
@@ -28,19 +30,55 @@ func NewModelCollectionOrdered[P Identifiable[ID], ID comparable](items []P) *Mo
 	return coll
 }
 
+func (c *ModelCollection[MP, ID]) Len() int {
+	return len(c.Map)
+}
+
+func (c *ModelCollection[MP, ID]) Has(id ID) bool {
+	_, ok := c.Map[id]
+	return ok
+}
+
 func (c *ModelCollection[MP, ID]) Find(id ID) (MP, bool) {
 	p, ok := c.Map[id]
 	return p, ok
 }
 
+func (c *ModelCollection[MP, ID]) IDs() []ID {
+	if len(c.Order) > 0 {
+		return append([]ID(nil), c.Order...) // preserve original order
+	}
+	ids := make([]ID, 0, len(c.Map))
+	for id := range c.Map {
+		ids = append(ids, id)
+	}
+	return ids
+}
+
+func (c *ModelCollection[MP, ID]) ForEach(fn func(MP)) {
+	for _, item := range c.Map {
+		fn(item)
+	}
+}
+
+func (c *ModelCollection[MP, ID]) ForEachOrdered(fn func(MP)) error {
+	if len(c.Order) == 0 {
+		return fmt.Errorf("collection is unordered")
+	}
+	for _, id := range c.Order {
+		fn(c.Map[id])
+	}
+	return nil
+}
+
 func LinkBelongsTo[
-	CP Identifiable[CID], CID comparable,
-	PP Identifiable[PID], PID comparable,
+CP Identifiable[CID], CID comparable,
+PP Identifiable[PID], PID comparable,
 ](
 	children *ModelCollection[CP, CID],
 	parents *ModelCollection[PP, PID],
 	foreignKeyFieldPtr func(CP) *PID, // on the child
-	relationFieldPtr func(CP) *PP, // on the child
+	relationFieldPtr func(CP) *PP,    // on the child
 ) {
 	for _, child := range children.Map {
 		if parent, ok := parents.Map[*foreignKeyFieldPtr(child)]; ok {
