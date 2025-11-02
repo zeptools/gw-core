8293bf3d732d6f96903c744bab14fbeb6820b96f
zeptools <zeptools.io@gmail.com>
2025-11-01 20:26:26 -0400

    .

--- orm/helpers.go
+++ orm/helpers.go
@@ -5,8 +5,8 @@ type Identifiable[ID comparable] interface {
 }
 
 func ModelPtrsToIDMap[
-P Identifiable[ID], // *Model struct
-ID comparable,
+	P Identifiable[ID], // *Model struct
+	ID comparable,
 ](itemptrs []P) map[ID]P {
 	idItemptrs := make(map[ID]P, len(itemptrs))
 	for _, p := range itemptrs {
