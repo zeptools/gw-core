beb8ad1f3285921c3cb8d712a210397f80276d3c
zeptools <zeptools.io@gmail.com>
2025-11-04 18:10:45 -0500

    .

--- db/sqldb/rawstore.go
+++ db/sqldb/rawstore.go
@@ -25,6 +25,16 @@ func (s *RawSQLStore) Get(key string) (string, bool) {
 	return stmt, exists
 }
 
+// GetOrPanic same as Get() but no `ok bool` but just panic
+// Use only when the key exists in the store
+func (s *RawSQLStore) GetOrPanic(key string) string {
+	stmt, exists := s.stmts[key]
+	if !exists {
+		log.Panicf("raw SQL not found for key: %s", key)
+	}
+	return stmt
+}
+
 func (s *RawSQLStore) GetAll() map[string]string {
 	return s.stmts
 }
