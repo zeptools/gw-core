d6c2ffc4e23ee194a2188723fb2ad13cd25e67ec
zeptools <zeptools.io@gmail.com>
2025-11-10 16:48:08 -0500

    .

--- conf/core_session_methods.go
+++ conf/core_session_methods.go
@@ -1,5 +1,11 @@
 package conf
 
+import "context"
+
 func (c *Core[B]) WebSessionIDToKVDBKey(sessionID string) string {
 	return c.AppName + "_wsession:" + sessionID
+}
+
+func (c *Core[B]) FindWebSessionInKVDB(ctx context.Context, sessionID string) (bool, error) {
+	return c.BackendKVDBClient.Exists(ctx, c.WebSessionIDToKVDBKey(sessionID))
 }
